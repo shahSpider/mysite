f98fc48523fca2bda11129574db2c0c24ca1efd3
shahspider@github.com <aq.syed007@gmail.com>
2024-08-14 22:21:35 +0500

    feat: add templates and url

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from leads.models import Lead, Agent
 
-# Create your views here.
+def home(request):
+    leads = Lead.objects.all()
+    context = {
+        "company": "Fashion 349",
+        "since": 2002,
+        "vision": """
+                        To inspire confidence and individuality through sustainable,
+                        cutting-edge fashion that transcends trends, empowering every person to express their unique style
+                        while fostering a positive impact on the world.
+                """,
+        "leads": leads
+    }
+    return render(request, 'leads/index.html', context)
